0a7d7bc5b63630371a3f6b7e042e795be3c24df1
jparcill <jparcill@edu.uwaterloo.ca>
2020-09-09 01:31:50 -0400

    Adding tests for audio.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,35 @@
 using MusicProcessing
-using Base.Test
+using SampledSignals: SampleBuf
+using Test
 
-# write your own tests here
-@test 1 == 1
+# Example audio
+audio_one_channel = SampleBuf(rand(1000), 10)
+audio_two_channel = SampleBuf(rand(1000,2), 10)
+audio_multi_channel = SampleBuf(rand(1000,2), 10)
 
+# audio.jl
 
+@testset "audio.jl" begin
+    @test size(mono(audio_two_channel)) == (1000,)
+    @test size(mono(audio_multi_channel)) == (1000,)
 
-include("MusicProcessing.jl")
-using MusicProcessing
-using SampledSignals
+    @test size(resample(audio_one_channel, 5))[1] == 500
+    @test size(resample(audio_two_channel, 5))[1] == 500
+    @test size(resample(audio_multi_channel, 5))[1] == 500
 
-# Example Vars
+    @test duration(audio_one_channel) == 100
+    @test duration(audio_two_channel) == 100
+    @test duration(audio_multi_channel) == 100
 
-audio = SampleBuf(rand(10), 0.5)
-# TFR
-spectrogram(audio)
+    @test duration(pitchshift(audio_one_channel)) == duration(audio_one_channel)
+    @test duration(pitchshift(audio_two_channel)) == duration(audio_two_channel)
+    @test duration(pitchshift(audio_multi_channel)) == duration(audio_multi_channel)
+
+    @test duration(speedup(audio_one_channel, 2)) ≈ duration(audio_one_channel) // 2
+    @test duration(speedup(audio_two_channel, 2)) ≈ duration(audio_two_channel) // 2
+    @test duration(speedup(audio_multi_channel, 2)) ≈ duration(audio_multi_channel) // 2
+
+    @test duration(slowdown(audio_one_channel, 2)) ≈ duration(audio_one_channel) * 2
+    @test duration(slowdown(audio_two_channel, 2)) ≈ duration(audio_two_channel) * 2
+    @test duration(slowdown(audio_multi_channel, 2)) ≈ duration(audio_multi_channel) * 2
+end
