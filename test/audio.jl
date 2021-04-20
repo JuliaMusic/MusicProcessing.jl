cb7704471fbff7e6518550e84797cbbb740d9c48
Ashwani Rathee <ab669522@gmail.com>
2021-04-20 20:48:40 +0530

    Fixes workflow

---
+++ test/audio.jl
@@ -0,0 +1,31 @@
+# audio.jl
+
+@testset "audio.jl" begin
+    # Example audio
+    audio_one_channel = SampleBuf(rand(1000), 10)
+    audio_two_channel = SampleBuf(rand(1000,2), 10)
+    audio_multi_channel = SampleBuf(rand(1000,2), 10)
+    
+    @test size(mono(audio_two_channel)) == (1000,)
+    @test size(mono(audio_multi_channel)) == (1000,)
+
+    @test size(resample(audio_one_channel, 5))[1] == 500
+    @test size(resample(audio_two_channel, 5))[1] == 500
+    @test size(resample(audio_multi_channel, 5))[1] == 500
+
+    @test duration(audio_one_channel) == 100
+    @test duration(audio_two_channel) == 100
+    @test duration(audio_multi_channel) == 100
+
+    @test duration(pitchshift(audio_one_channel)) == duration(audio_one_channel)
+    @test duration(pitchshift(audio_two_channel)) == duration(audio_two_channel)
+    @test duration(pitchshift(audio_multi_channel)) == duration(audio_multi_channel)
+
+    @test duration(speedup(audio_one_channel, 2)) ≈ duration(audio_one_channel) // 2
+    @test duration(speedup(audio_two_channel, 2)) ≈ duration(audio_two_channel) // 2
+    @test duration(speedup(audio_multi_channel, 2)) ≈ duration(audio_multi_channel) // 2
+
+    @test duration(slowdown(audio_one_channel, 2)) ≈ duration(audio_one_channel) * 2
+    @test duration(slowdown(audio_two_channel, 2)) ≈ duration(audio_two_channel) * 2
+    @test duration(slowdown(audio_multi_channel, 2)) ≈ duration(audio_multi_channel) * 2
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,35 +1,19 @@
+module MusicProcessingTest
+
 using MusicProcessing
 using SampledSignals: SampleBuf
 using Test
 
-# Example audio
-audio_one_channel = SampleBuf(rand(1000), 10)
-audio_two_channel = SampleBuf(rand(1000,2), 10)
-audio_multi_channel = SampleBuf(rand(1000,2), 10)
+tests = [
+    "audio.jl",
+]
 
-# audio.jl
-
-@testset "audio.jl" begin
-    @test size(mono(audio_two_channel)) == (1000,)
-    @test size(mono(audio_multi_channel)) == (1000,)
-
-    @test size(resample(audio_one_channel, 5))[1] == 500
-    @test size(resample(audio_two_channel, 5))[1] == 500
-    @test size(resample(audio_multi_channel, 5))[1] == 500
-
-    @test duration(audio_one_channel) == 100
-    @test duration(audio_two_channel) == 100
-    @test duration(audio_multi_channel) == 100
-
-    @test duration(pitchshift(audio_one_channel)) == duration(audio_one_channel)
-    @test duration(pitchshift(audio_two_channel)) == duration(audio_two_channel)
-    @test duration(pitchshift(audio_multi_channel)) == duration(audio_multi_channel)
-
-    @test duration(speedup(audio_one_channel, 2)) ≈ duration(audio_one_channel) // 2
-    @test duration(speedup(audio_two_channel, 2)) ≈ duration(audio_two_channel) // 2
-    @test duration(speedup(audio_multi_channel, 2)) ≈ duration(audio_multi_channel) // 2
-
-    @test duration(slowdown(audio_one_channel, 2)) ≈ duration(audio_one_channel) * 2
-    @test duration(slowdown(audio_two_channel, 2)) ≈ duration(audio_two_channel) * 2
-    @test duration(slowdown(audio_multi_channel, 2)) ≈ duration(audio_multi_channel) * 2
+for t in tests
+    @testset "$t" begin
+        include(t)
+    end
 end
+
+end
+
+
