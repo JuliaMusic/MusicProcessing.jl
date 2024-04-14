9c1ed55c3eb1078d8e9aed31e8693eb43152475f
Albert R. Gnadt <20693045+gnadt@users.noreply.github.com>
2024-04-14 17:08:18 -0400

    Clean up audio test file

--- test/audio.jl
+++ test/audio.jl
@@ -1,71 +1,58 @@
 # audio.jl
 
-@testset verbose= true "audio.jl" begin
-    
+@testset verbose = true "audio.jl" begin
+
     # Example audio
-    audio_one_channel = SampleBuf(rand(1000), 10)
-    audio_two_channel = SampleBuf(rand(1000,2), 10)
-    audio_multi_channel = SampleBuf(rand(1000,2), 10)
-    
-    # Tests for mono
+    audio_one   = SampleBuf(rand(1000  ), 10)
+    audio_two   = SampleBuf(rand(1000,2), 10)
+    audio_multi = SampleBuf(rand(1000,3), 10)
+
     @testset "mono" begin
-        @test size(mono(audio_two_channel)) == (1000,)
-        @test size(mono(audio_multi_channel)) == (1000,)
-        
+        for audio in [audio_one, audio_two, audio_multi]
+            @test size(mono(audio)) == (1000,)
+        end
     end
 
-    # Tests for resample method
-    @testset "resample.jl" begin
-        @test size(resample(audio_one_channel, 5))[1] == 500
-        @test size(resample(audio_two_channel, 5))[1] == 500
-        @test size(resample(audio_multi_channel, 5))[1] == 500
+    @testset "resample" begin
+        for audio in [audio_one, audio_two, audio_multi]
+            @test size(resample(audio, 5), 1) == 500
+        end
     end
 
-    # Tests for duration method
     @testset "duration" begin
-        @test duration(audio_one_channel) == 100
-        @test duration(audio_two_channel) == 100
-        @test duration(audio_multi_channel) == 100
+        for audio in [audio_one, audio_two, audio_multi]
+            @test duration(audio) == 100
+        end
     end
-    audio_one_channel = SampleBuf(rand(10000), 1000)
-    audio_two_channel = SampleBuf(rand(10000,2), 1000)
-    audio_multi_channel = SampleBuf(rand(10000,2), 1000)
-    # Tests for pitchshift method
+
+    # Example audio
+    audio_one   = SampleBuf(rand(10000  ), 1000)
+    audio_two   = SampleBuf(rand(10000,2), 1000)
+    audio_multi = SampleBuf(rand(10000,3), 1000)
+
     @testset "pitchshift" begin
-        @test isapprox(duration(pitchshift(audio_one_channel)), duration(audio_one_channel), atol=0.2)
-        @test isapprox(duration(pitchshift(audio_two_channel)), duration(audio_two_channel), atol=0.2)
-        @test isapprox(duration(pitchshift(audio_multi_channel)), duration(audio_multi_channel), atol=0.2)
+        for audio in [audio_one, audio_two, audio_multi]
+            @test isapprox(duration(pitchshift(audio)), duration(audio), atol=0.2)
+        end
     end
 
-    audio_one_channel = SampleBuf(rand(10000), 1000)
-    audio_two_channel = SampleBuf(rand(10000,2), 1000)
-    audio_multi_channel = SampleBuf(rand(10000,2), 1000)
-    # Tests for speedup method
     @testset "speedup" begin
-        
-        @test isapprox(duration(speedup(audio_one_channel, 2)), duration(audio_one_channel) / 2.0 , atol=0.4)
-        @test isapprox(duration(speedup(audio_two_channel, 2)), duration(audio_two_channel) / 2.0 , atol=0.4)
-        @test isapprox(duration(speedup(audio_multi_channel, 2)), duration(audio_multi_channel) / 2.0 , atol=0.4)
+        for audio in [audio_one, audio_two, audio_multi]
+            @test isapprox(duration(speedup(audio, 2)), duration(audio) / 2.0 , atol=0.4)
+        end
     end
 
-    audio_one_channel = SampleBuf(rand(10000), 1000)
-    audio_two_channel = SampleBuf(rand(10000,2), 1000)
-    audio_multi_channel = SampleBuf(rand(10000,2), 1000)
-    # Tests for slowdown method
     @testset "slowdown" begin
+        for audio in [audio_one, audio_two, audio_multi]
+            @test isapprox(duration(slowdown(audio, 2)), duration(audio) * 2.0, atol=1.2)
+        end
+    end
 
-        @test isapprox(duration(slowdown(audio_one_channel, 2)), duration(audio_one_channel) * 2.0, atol=1.2)
-        @test isapprox(duration(slowdown(audio_two_channel, 2)), duration(audio_two_channel) * 2.0, atol=1.2)
-        @test isapprox(duration(slowdown(audio_multi_channel, 2)), duration(audio_multi_channel) * 2.0, atol=1.2)
-   end
+    # Example audio
+    audio_one = SampleBuf(rand(100000), 10)
 
-    # Tests for zero crossing rate method
     @testset "zero_crossing_rate" begin
-        audio_one_channel = SampleBuf(rand(100000), 10)
-
-        expected = length(zero_crossing_rate(audio_one_channel, 2048, 512))
-        res = 192
-        @test expected == res
+        @test length(zero_crossing_rate(audio_one, 2048, 512)) == 192
     end
 
 end
